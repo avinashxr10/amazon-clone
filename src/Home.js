d1acaddb7b6d76dc277f3b228bd252222337945d
Avinash Kumar <avinashxr10@gmail.com>
2022-06-15 21:11:17 +0530

    Home Page

---
+++ src/Home.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt=""
+        />
+
+        <div className="home__row">
+          <Product
+            title="The Lean Startup"
+            price={29.99}
+            image="https://m.media-amazon.com/images/P/0670921602.01._SCLZZZZZZZ_SX500_.jpg"
+            rating={3}
+          />
+          <Product
+            title="The Lean Startup"
+            price={29.99}
+            image="https://m.media-amazon.com/images/P/0670921602.01._SCLZZZZZZZ_SX500_.jpg"
+            rating={3}
+          />
+        </div>
+
+        <div className="home__row">
+          <Product
+            id="4903850"
+            title="The Lean Startup"
+            price={29.99}
+            image="https://m.media-amazon.com/images/P/0670921602.01._SCLZZZZZZZ_SX500_.jpg"
+            rating={3}
+          />
+          <Product
+            title="The Lean Startup"
+            price={29.99}
+            image="https://m.media-amazon.com/images/P/0670921602.01._SCLZZZZZZZ_SX500_.jpg"
+            rating={3}
+          />
+          <Product
+            title="The Lean Startup"
+            price={29.99}
+            image="https://m.media-amazon.com/images/P/0670921602.01._SCLZZZZZZZ_SX500_.jpg"
+            rating={3}
+          />
+        </div>
+
+        <div className="home__row">
+          <Product
+            title="The Lean Startup"
+            price={29.99}
+            image="https://m.media-amazon.com/images/P/0670921602.01._SCLZZZZZZZ_SX500_.jpg"
+            rating={3}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCuZOj9qRjDM5ZZeBKsYF3tkMbM2EZMFsA",
-    authDomain: "challenge-e3e63.firebaseapp.com",
-    databaseURL: "https://challenge-e3e63-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "challenge-e3e63",
-    storageBucket: "challenge-e3e63.appspot.com",
-    messagingSenderId: "830406681561",
-    appId: "1:830406681561:web:3c1f5f303fa35d5c546fc8",
-    measurementId: "G-71WMZ5KYR5"
-  };
+  apiKey: "AIzaSyCuZOj9qRjDM5ZZeBKsYF3tkMbM2EZMFsA",
+  authDomain: "challenge-e3e63.firebaseapp.com",
+  databaseURL:
+    "https://challenge-e3e63-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "challenge-e3e63",
+  storageBucket: "challenge-e3e63.appspot.com",
+  messagingSenderId: "830406681561",
+  appId: "1:830406681561:web:3c1f5f303fa35d5c546fc8",
+  measurementId: "G-71WMZ5KYR5",
+};
